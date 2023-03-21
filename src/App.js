c77b5e87490c2706e0debcf0cc7d00b8228fdd17
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-03-21 17:03:34 +0545

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Discounts from './pages/Discounts';
+import ForgotPassWord from './pages/ForgotPassWord';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
 
 function App() {
   return (
     <div className="App">
-     <h1>Sup</h1>
+     <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-out' element={<SignOut/>}/>
+          <Route path='/forgot-password' element={<ForgotPassWord/>}/>
+          <Route path='/discounts' element={<Discounts/>}/>
+        </Routes>
+     </Router>
     </div>
   );
 }
---
+++ src/pages/ForgotPassWord.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ForgotPassWord() {
+  return (
+    <div>ForgotPassWord</div>
+  )
+}
