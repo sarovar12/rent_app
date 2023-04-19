bd8e281c2c144a5cfe073feab960d239bdd22fca
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-04-19 18:49:03 +0545

    Spinner Component for Loading Profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
         }
     }
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-20'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div className='flex '>
                 <img
