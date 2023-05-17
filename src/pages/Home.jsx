04ad7b801dd2c30da3db35c503fa44bc4e4ebda3
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-05-17 17:25:10 +0545

    Added Rent and Sale Section in homepage and removed ugly scrollbar from the app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -114,6 +114,51 @@ useEffect(()=>{
 
         )}
       </div>
+
+      {/* Rent Section */}
+      <div>
+      {offerListings && rentListing.length>0  && (
+          <div className='m-2 mb-6'>
+              <h2 className='px-3 text-2xl mt-6
+               font-semibold'> Places for Rent</h2> 
+            <Link to='/category/rent'>
+                <p className='px-3 text-sm text-blue-600
+                hover:text-blue-700
+                transition duration-150
+                ease-in-out'>Show more places for rent</p>      
+            </Link>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+              {rentListing.map((listing)=>(
+                  <ListingItem key={listing.id} listing={listing.data}
+                  id={listing.id}/>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
+            {/* Sale Section */}
+            <div>
+      {offerListings && saleListing.length>0  && (
+          <div className='m-2 mb-6'>
+              <h2 className='px-3 text-2xl mt-6
+               font-semibold'> Places for Sale</h2> 
+            <Link to='/category/sale'>
+                <p className='px-3 text-sm text-blue-600
+                hover:text-blue-700
+                transition duration-150
+                ease-in-out'>Show more places for sale</p>      
+            </Link>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+              {saleListing.map((listing)=>(
+                  <ListingItem key={listing.id} listing={listing.data}
+                  id={listing.id}/>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
     </div>)
 
 }
