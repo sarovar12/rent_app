445ccf81549d8b6c4c28ea33887892313fb3c00c
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-05-22 18:54:25 +0545

    Fixed error paragraph in Category

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -98,7 +98,7 @@ async function onFetchMoreListings(){
           }
         </>
       ) : (
-        <p> There are no current offers! </p>
+        <p> There are no current {params.categoryName === 'rent' ? "places for rent" : "places for sale"} </p>
       )
 }
       </div>
